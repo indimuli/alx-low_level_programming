bad8851ce7b672cf3ae7480387317d05e81a10fb
indimuli <indimulih504@gmail.com>
2023-06-15 18:52:04 -0700

    gggggggg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
-* * main - Prints out a string to stdout.
+* * Description: main - Prints out a string to stdout.
 * * Return: 0 if success.
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 Return(0);
-}
